5c954a5aa557e6c4cfe0a694e17106773ceb300d
Tom Humphris <tom.humphris@switchconcepts.com>
2017-12-14 15:16:24 +0000

    Import element components on demand with a babel loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,30 +11,47 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt Playground' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   /**
    * Customize the progress bar color
    */
   loading: { color: '#000' },
   /**
-   * 
+   *
    */
   plugins: [
-    'element-ui'
+    //
   ],
   /**
    * Build configuration (webpack extension)
    */
   build: {
-    extend (config, ctx) {
-      // SASS loader pipeline for Vue single file components
-      const vueLoader = config.module.rules.find((rule) => rule.loader === 'vue-loader')
-      vueLoader.options.loaders.scss = 'vue-style-loader!css-loader!sass-loader?' + JSON.stringify({ includePaths: [path.resolve(__dirname), 'node_modules'] })
+    /*
+    ** Babel
+     */
+    babel: {
+      presets: ['vue-app'],
+      plugins: [
+        [
+          'component',
+          [
+            {
+              libraryName: 'element-ui',
+              styleLibraryName: 'theme-chalk'
+            }
+          ]
+        ]
+      ],
+      comments: false
+    },
 
-      // Run ESLint on build
+    extend(config, ctx) {
+      // SASS loader pipeline for Vue single file components
+      const vueLoader = config.module.rules.find(rule => rule.loader === 'vue-loader');
+      vueLoader.options.loaders.scss = 'vue-style-loader!css-loader!sass-loader?' + JSON.stringify({includePaths: [path.resolve(__dirname), 'node_modules']});
+
+      // Run ESLint on save
       if (ctx.dev && ctx.isClient) {
         config.module.rules.push({
           enforce: 'pre',
