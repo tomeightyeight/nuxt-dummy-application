de9832f695a8cb6dc6538ccc2d2c67d502e4032a
Tom Humphris <tom@muska.co.uk>
2017-12-18 12:55:05 +0000

    Convert to 2 spaces

--- test/store/Todos.spec.js
+++ test/store/Todos.spec.js
@@ -1,40 +1,39 @@
 /* eslint-disable */
 import {
-    state, 
-    types,
-    getters,
-    mutations
+  state, 
+  types,
+  getters,
+  mutations
 } from '@/store/todos.js'
 
 let baseStore
 
 beforeEach(() => {
-    baseStore = state();
+  baseStore = state();
 })
 
 describe('mutations', () => {
-    it('adds a todo to the list array', () => {
-        const text = 'foo'        
-        mutations[types.ADD_TODO](baseStore, { text: text })
-        const item = baseStore.list.pop()
+  it('adds a todo to the list array', () => {
+    const text = 'foo'
+    mutations[types.ADD_TODO](baseStore, { text: text })
+    const item = baseStore.list.pop()
 
-        expect(item.text).toEqual(text)
-    })
+    expect(item.text).toEqual(text)
+  })
 
-    it('removes a todo from the list array by index', () => {
-        const first = baseStore.list[0]
-        mutations[types.REMOVE_TODO](baseStore, { index: 0 })
-        
-        expect(first.text).not.toEqual(baseStore.list[0].text)
-
-    })
+  it('removes a todo from the list array by index', () => {
+    const first = baseStore.list[0]
+    mutations[types.REMOVE_TODO](baseStore, { index: 0 })
+    
+    expect(first.text).not.toEqual(baseStore.list[0].text)
+  })
 })
 
 describe('getters', () => {
-    it('returns a count of all the completed todos', () => {
-        const actual = baseStore.list.filter(todo => todo.completed).length
-        const completed = getters.completedToDos(baseStore)
+  it('returns a count of all the completed todos', () => {
+    const actual = baseStore.list.filter(todo => todo.completed).length
+    const completed = getters.completedToDos(baseStore)
 
-        expect(completed).toEqual(actual)
-    })   
+    expect(completed).toEqual(actual)
+  })   
 })
