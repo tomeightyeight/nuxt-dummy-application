73fb1eb32c64f901a83ec232096d358a30d1a1a8
Tom Humphris <tom@muska.co.uk>
2017-12-18 09:31:14 +0000

    Add a base dummy test spec for Todos store

---
+++ test/store/Todos.spec.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { mutations } from '@/store/todos.js'
+
+console.log(mutations)
+
+describe('mutations', () => {
+    it('adds a todo to the list array', () => {
+        //
+    })
+})
